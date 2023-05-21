e56f367ff8ba1c01e2875c9c821eddc320fa80e6
wdring <when5310@163.com>
2023-05-21 15:56:01 +0800

    Update base.js

--- js/base.js
+++ js/base.js
@@ -709,8 +709,9 @@ await CONTS[name].balanceOf(adr)
 async function updataData_dashbord() {
     let totalSupply = await CONTS['web3'].totalSupply() / ETHDIV;
     let totalStaked = await CONTS['chef'].totalStaked();
+    let balchef = await CONTS['web3'].balanceOf(ADRS["chef"]);
     let zif = await CONTS['web3'].balanceOf(ADRS["zif"]);
-    let circulatingSupply = totalSupply;
+    let circulatingSupply = totalSupply - balchef + totalStaked;
     let mcap = price * circulatingSupply;
     displayText("#cirSupply", `${COMMA(ROUND(circulatingSupply, 2))}`);
     displayText("#stakedRate", `$${COMMA(ROUND(totalStaked / circulatingSupply * 100, 2))}%`);
